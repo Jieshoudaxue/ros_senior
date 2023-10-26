f08006811036f6b09bd91bc62ceabce6dd25878f
jieshoudaxue <1641395022@qq.com>
2023-10-26 10:21:43 +0800

    [feat] add action_sample

---
+++ action_sample/src/DoDishes_client.cpp
@@ -0,0 +1,6 @@
+#include <actionlib/client/simple_action_client.h>
+#include "action_sample/DoDishesAction.h"
+
+int main(int argc, char** argv) {
+  return 0;
+}
---
+++ action_sample/src/DoDishes_server.cpp
@@ -0,0 +1,7 @@
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include "action_sample/DoDishesAction.h"
+
+int main(int argc, char** argv) {
+  return 0;
+}
